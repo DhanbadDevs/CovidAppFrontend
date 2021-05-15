760ad1fcf603ebadea2c4dddc95d82e8acc28ea0
snehal2398 <snehal2398@gmail.com>
2021-05-15 09:47:59 +0530

    Improved UI

--- src/Components/BedCard.jsx
+++ src/Components/BedCard.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import Bed from '../Assets/bed.png'
 import moment from 'moment'
-const BedCard =(props)=>{
-    return(
+const BedCard = (props) => {
+    return (
         <div className="col-12 mb-3">
-        <div className="row shadow" style={{position:'relative',backgroundColor:"#E4F7FF",padding:'20px 0',width:'100%',margin:'auto',borderRadius:'10px'}}>
-        
-        <div className="col-4 p-0" style={{display:'flex',flexDirection:'column',justifyContent: 'flex-end',alignItems: 'stretch'}}>
-            <img src={Bed} style={{width:'50px',alignSelf:'center',marginBottom:'10px'}}/>
-            <div style={{backgroundColor:"#55C3DB" ,margin:'5px',padding:'3px 10px',borderRadius:'20px',fontSize:'14px'}}>Cost:{props.item.charges}</div>
-            <div style={{backgroundColor:"#FE9574" ,margin:'5px',padding:'3px 10px',borderRadius:'20px',fontSize:'14px'}}>{props.item.phone}</div>
-        </div>
-        <div className="col-8 m-0 mb-1" style={{backgroundColor:'#BFE9FB',marginBottom:'20px',width:'100%',borderRadius:'10px'}}>
-            <p style={{textAlign:'left',fontSize:'14px'}}>Bed Type:{props.item.bedType}<br/>
-            Bed Count:{props.item.bedCount}<br/>
-            Hospital:{props.item?.location?.locationName}<br/>
-            Private:{props.item?.location?.isPrivate?"true":"false"}<br/>
+            <div className="row shadow" style={{ position: 'relative', backgroundColor: "#E4F7FF", padding: '20px 0', width: '100%', margin: 'auto', borderRadius: '10px' }}>
+
+                <div className="col-4 p-0" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'stretch' }}>
+                    <img src={Bed} style={{ width: '50px', alignSelf: 'center', marginBottom: '10px' }} />
+                    <div style={{ backgroundColor: "#55C3DB", margin: '5px', padding: '3px 10px', borderRadius: '20px', fontSize: '14px' }}>Cost:{props.item.charges}</div>
+                    <div style={{ backgroundColor: "#FE9574", margin: '5px', padding: '3px 10px', borderRadius: '20px', fontSize: '14px' }}>{props.item.phone}</div>
+                </div>
+                <div className="col-8 m-0 mb-1" style={{ backgroundColor: '#BFE9FB', marginBottom: '20px', width: '100%', borderRadius: '10px' }}>
+                    <p style={{ textAlign: 'left', fontSize: '14px' }}>Bed Type:{props.item.bedType}<br />
+            Bed Count:{props.item.bedCount}<br />
+            Hospital:{props.item?.location?.locationName}<br />
+            Private:{props.item?.location?.isPrivate ? "true" : "false"}<br />
             Address:{props.item?.location?.address}
-            </p>
-        {props.item.isVerified && <i style={{color: '#09B22E',position:'absolute',top:3,right:10}} class="fas fa-check-circle"></i>}
-        </div>
-        <i style={{position:'absolute',bottom:5,right:10}} class="fal fa-thumbs-up"><span className='pl-1'>20{props.item.votes}</span></i>
-        <p style={{position:'absolute',bottom:0,left:'40%',margin:0,fontSize:'10px'}}>Updated On:{moment(props.item.updatedOn).format('DD/MM/YYYY') }</p>
-        </div>
+                    </p>
+                    {props.item.isVerified && <i style={{ color: '#09B22E', position: 'absolute', top: 3, right: 10 }} class="fas fa-check-circle"></i>}
+                </div>
+                <i style={{ position: 'absolute', bottom: 5, right: 10 }} class="fal fa-thumbs-up"></i><span className='pl-1'>20{props.item.votes}</span>
+                <i style={{ position: 'absolute', bottom: 5, right: 10 }} class="fal fa-thumbs-downs"></i>
+                <p style={{ position: 'absolute', bottom: 0, left: '40%', margin: 0, fontSize: '10px' }}>Updated On:{moment(props.item.updatedOn).format('DD/MM/YYYY')}</p>
+            </div>
         </div>
     )
 }
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,58 +1,89 @@
 import React from 'react'
 import BedCard from '../Components/BedCard'
 import ItemCard from '../Components/ItemCard'
-const HomePage=()=>{
-    const items=[
+import Navbar from 'react-bootstrap/Navbar'
+const HomePage = () => {
+    const items = [
         {
-            title:'Hospital & Beds',
-            icon:"fas fa-procedures",
-            to:'/bedtype'
+            id: 1,
+            colourId: 0,
+            title: 'Hospital & Beds',
+            icon: "fas fa-procedures",
+            to: '/bedtype'
         },
         {
-            title:'Medicine & Equipment',
-            icon:"fas fa-pills",
-            to:'/medtype'
+            id: 2,
+            colourId: 1,
+            title: 'Medicine & Equipment',
+            icon: "fas fa-pills",
+            to: '/medtype'
         },
         {
-            title:'Vaccine',
-            icon:"fas fa-syringe",
-            to:'/vaccinelist'
+            id: 3,
+            colourId: 1,
+            title: 'Vaccine',
+            icon: "fas fa-syringe",
+            to: '/vaccinelist'
         },
-        
+
         {
-            title:'Blood & Plasma',
-            icon:"fas fa-tint",
-            to:'/bloodplasmatype'
-        },
-        {
-            title:'NGO',
-            icon:"fas fa-hands-helping",
-            to:'/ngolist'
-        },
-        {
-            title:'Guidelines',
-            icon:"fas fa-clipboard-list-check",
-            to:'/guidelinelist'
+            id: 4,
+            colourId: 0,
+            title: 'Blood & Plasma',
+            icon: "fas fa-tint",
+            to: '/bloodplasmatype'
         },
         {
-            title:'Doctors',
-            icon:"fas fa-user-md",
-            to:'/doctorslist'
+            id: 5,
+            colourId: 0,
+            title: 'NGO',
+            icon: "fas fa-hands-helping",
+            to: '/ngolist'
         },
         {
-            title:'Ambulance',
-            icon:"fad fa-ambulance",
-            to:'/ambulancelist'
+            id: 6,
+            colourId: 1,
+            title: 'Guidelines',
+            icon: "fas fa-clipboard-list-check",
+            to: '/guidelinelist'
+        },
+        {
+            id: 7,
+            colourId: 1,
+            title: 'Doctors',
+            icon: "fas fa-user-md",
+            to: '/doctorslist'
+        },
+        {
+            id: 8,
+            colourId: 0,
+            title: 'Ambulance',
+            icon: "fad fa-ambulance",
+            to: '/ambulancelist'
         },
     ]
-    return(
-        <div className="container py-1" style={{display: 'flex',justifyContent: 'center',alignItems: 'center',flex:1}}>
-        <div className="row">
-            {items.map(item=>
-                <ItemCard item={item}/>
-            )}
+    return (
+        <div>
+            <Navbar bg="custom" variant="blue">
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src="https://www.eurodiaconia.org/wordpress/wp-content/uploads/2020/03/hilf-jetzt-logo-ohne-text.png"
+                        width="40"
+                        height="40"
+                        className="d-inline-block align-top mt-1"
+                    />{' '}
+                    <h1 style={{ color: "#0A1048", }} className='d-inline-block' >CovidApp</h1>
+                </Navbar.Brand>
+            </Navbar>
+            <div className="container-fluid flex-column mt-3 ">
+                <div className="row">
+                    {items.map(item =>
+                        <ItemCard item={item} />
+                    )}
 
-        </div>
+                </div>
+            </div>
         </div>
     )
 }
