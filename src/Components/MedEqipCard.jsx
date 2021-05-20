456f9e138f2a9025c57198d63e66fde6079ae6c8
Neerajistheboss <neerajistheboss@gmail.com>
2021-05-20 21:49:35 +0530

    changes

--- src/Components/MedEqipCard.jsx
+++ src/Components/MedEqipCard.jsx
@@ -4,7 +4,7 @@ import moment from 'moment'
 const MedEquipCard =(props)=>{
     return(
         <div className="col-12 mb-3">
-        <div className="row shadow" style={{position:'relative',backgroundColor:"#E4F7FF",padding:'20px 0',width:'100%',margin:'auto',borderRadius:'10px'}}>
+        <div className="row shadow pr-1" style={{position:'relative',backgroundColor:"#E4F7FF",padding:'20px 0',width:'100%',margin:'auto',borderRadius:'10px'}}>
         
         <div className="col-4 p-0" style={{display:'flex',flexDirection:'column',justifyContent: 'flex-end',alignItems: 'stretch'}}>
             <img src={Bed} style={{width:'50px',alignSelf:'center',marginBottom:'10px'}}/>
@@ -14,21 +14,24 @@ const MedEquipCard =(props)=>{
             )}
         </div>
         <div className="col-8 m-0 mb-1" style={{backgroundColor:'#BFE9FB',marginBottom:'20px',width:'100%',borderRadius:'10px'}}>
-            <p style={{textAlign:'left',fontSize:'14px'}}>
+            <p style={{textAlign:'left',fontSize:'14px',textAlign:'center',margin:0}}>
+            <span style={{fontWeight:'bold'}}>{props.item?.location?.locationName}</span><br/>
+            <span>{props.item?.location?.address}</span>
+            </p>
+            <p>        
             Stock:{props.item.stock}<br/>
             <span style={{}}>HomeDelivery:{props.item?.isAirConditioned?<i class="fas fa-check text-success pl-2"></i>:<i class="fas fa-times text-danger pl-2"></i>}</span><br/>
             Timing:{props.item.timing}<br/>
-            Hospital:{props.item?.location?.locationName}<br/>
-            Address:{props.item?.location?.address}<br/>
             Note:{props.item.notes}
             </p>
-        {props.item.isVerified && <p className="verified" >
-            Verified
-        <i  class="fas fa-check-circle"></i>
-        </p>}
+        
         </div>
         {/* <i style={{position:'absolute',bottom:5,right:10}} class="fal fa-thumbs-up"><span className='pl-1'>20{props.item.votes}</span></i> */}
         <p style={{position:'absolute',bottom:0,left:'40%',margin:0,fontSize:'10px'}}>Updated On:{moment(props.item.updatedOn).format('DD/MM/YYYY') }</p>
+        {props.item.isVerified && <p className="verified" >
+        <i  class="fas fa-check-circle"></i>
+            Verified
+        </p>}
         </div>
         </div>
     )
