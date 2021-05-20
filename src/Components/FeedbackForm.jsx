d2ea5e1474adea5b9cc0bf09e6f830d80b13a7ec
aman shaw <amansahw4511@gmail.com>
2021-05-20 22:06:12 +0530

    form

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -14,7 +14,7 @@ const FeedbackFrom = (props) => {
         }}
       >
       <div
-      className="px-5 py-3 "
+      className="px-4 py-3 "
       style={{
         backgroundColor: "#BFE9FB",
         width: "100%",
@@ -25,21 +25,23 @@ const FeedbackFrom = (props) => {
         <form>
             <div className="form-group row">
                 <label htmlFor="name" className="col-sm-3" > Name </label>
-            <input type="text" className="form-control col-sm" id="name"  required />
+            <input type="text" style={{ borderRadius : "10px"}} className="form-control col-sm" id="name" required/>
             </div>
             <div className="form-group row">
                 <label htmlFor="email" className="col-sm-3" > Email </label>
-            <input type="email" className="form-control col-sm" placeholder="name@exaple.com" id="email" required />
+            <input type="email" style={{ borderRadius : "10px"}} className="form-control col-sm" placeholder="name@exaple.com" id="email" required />
             </div>
             <div className="form-group row">
                 <label htmlFor="phone" className="col-sm-3"> Phone </label>
-            <input type="phone" className="form-control col-sm" placeholder="987654321" id="phone" />
+            <input type="phone" style={{ borderRadius : "10px"}} className="form-control col-sm" placeholder="987654321" id="phone" />
             </div>
             <div className="form-group row">
                 <label htmlFor="description" className="col-sm-3"> Description </label>
-                <textarea className="form-control col-sm" id="description" ></textarea>
+                <textarea className="form-control col-sm" style={{ borderRadius : "10px"}} id="description" ></textarea>
             </div>
-            <button type="submit" className="btn btn-primary row text-right mt-3" style={{float:"right", clear:"both"}} > Submit</button>
+            <div className="text-right">
+            <button type="submit" className="btn btn-primary row my-2"> Submit</button>
+        </div>
         </form>
     </div>
 </div>
