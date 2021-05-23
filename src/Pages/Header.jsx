c91a127b8cddb3c4865b326fd447d2ed7defefd8
ss24910 <snehal.singh@zs.com>
2021-05-23 18:23:55 +0530

    added header text

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -7,24 +7,34 @@ import Back from '../Assets/back.png'
 
 const  Header=(props)=> {
     let history = useHistory();
-      
+       var heading;
+       switch(window.location.pathname){
+           case "/ambulancelist": heading='AMBULANCE';break;
+           case "/bedlist":heading="HOSPITAL BEDS";break;
+           case "/medtype":heading="MEDICINE & EQUIPMENT";break;
+           case "/oxygenlist":heading="OXYGEN";break;
+           case "/doctorslist":heading="DOCTOR";break;
+           case "/helplinelist":heading="HELPLINE";break;
+           default :heading="COVID APP"
+       }
+       if(window.location.pathname.includes("/medlist"))
+       heading="MEDICINE & EQUIPMENT"
     return(
-<Navbar bg="custom" variant="blue">
+<Navbar bg="custom" variant="blue" className="py-0">
                 <Navbar.Brand >
                 <div className="align-middle">
-                  <div className="d-inline-block align-middle mb-3">
+                  <div className="d-inline-block align-middle mb-2">
                     <img
                         alt=""
                         src="https://www.eurodiaconia.org/wordpress/wp-content/uploads/2020/03/hilf-jetzt-logo-ohne-text.png"
-                        width="40"
-                        height="40"
+                        width="35"
+                        height="35"
                         className=""
                         style={{display:(window.location.pathname==="/")?"block":"none"}}
                     />
                     <img src={Back}  onClick={() => history.goBack()} style={{display:(window.location.pathname!=="/")?"block":"none",fontSize:"40px"}}/>
                     </div>
-                    {'    '}
-                    <h1 style={{ color: "#FFF"}} className='d-inline-block' >CovidApp</h1>
+                    <h4 style={{ color: "#FFF"}} className='d-inline-block ml-3' >{heading}</h4>
                 </div>
                 </Navbar.Brand>
             </Navbar>
